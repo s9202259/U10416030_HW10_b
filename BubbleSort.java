0c693fcda21732c9139c9bc6ac9d47b149a7c8f0
s9202259 <s9202259@yahoo.com.tw>
2016-04-11 15:11:32 +0800

    complete program
    add class Bubble function
    add methods

--- BubbleSort.java
+++ BubbleSort.java
@@ -14,9 +14,38 @@ public class BubbleSort {
 			System.out.println("Enter number: ");
 			bubble[i] = input.nextInt();
 		}
+		
+		Bubble sort = new Bubble(bubble);
+		
+		for (int n = 0; n < howMany; n++) {
+			sort.printSort(n);
+		}
 	}
 }
 
 class Bubble {
+	int arrayLength;
+	int[] sortArray = new int[arrayLength];
+	int h;
 	
+	Bubble(int array[]) {
+		arrayLength = array.length;
+		sortArray = array;
+		cal();
+	}
+	
+	public void cal() {
+		for (int i = 0; i < arrayLength; i++) {
+			for (int j = 0; j < arrayLength - i; j++) {
+				if (sortArray[j] < sortArray[j + 1]) {
+					h = sortArray[j + 1];
+					sortArray[j + 1] = sortArray[j];
+					sortArray[j] = h;
+				}
+			}
+		}
+	}
+	public void printSort(int n) {
+		System.out.println(sortArray[n]);
+	}
 }
